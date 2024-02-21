a3855d77baad36afbee306281e9a4a404cf2e7be
Nitoich <kargaev@web2cat.ru>
2024-02-21 14:46:03 +0300

    Update supabase payment status

--- server/api/notifyPayment.js
+++ server/api/notifyPayment.js
@@ -1,11 +1,21 @@
 import * as fs from "fs";
+import {createClient} from "@supabase/supabase-js";
 
 export default defineEventHandler(async (event) => {
     let body = await readBody(event);
-    const query = getQuery(event);
-    fs.writeFile("notify.log", JSON.stringify({
-        body,
-        query
-    }), (err) => {});
-    console.log(body, query);
+    body = JSON.parse(body);
+    const supabase = createClient("https://ajaijburhzihuzlqldup.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqYWlqYnVyaHppaHV6bHFsZHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1MTA5OTgsImV4cCI6MjAyNDA4Njk5OH0.H1hQLcR46arQEMmL0a-C2rvW_JU7DMJ4AWEZ-6BANd8");
+    console.log(body);
+    switch (body.Status) {
+        case "CONFIRMED":
+            await supabase.from("Payments")
+                .update({ is_paid: true, pan: body.Pan, cardExpDate: body.ExpDate, status: "CONFIRMED" })
+                .eq("id", body.OrderId);
+            break;
+        case "REJECTED":
+            await supabase.from("MovieBookingSeat").delete().eq("payment_id", body.OrderId);
+            await supabase.from("Payments")
+                .update({ pan: body.Pan, cardExpDate: body.ExpDate, status: "REJECT" })
+                .eq('id', body.OrderId);
+    }
 });
